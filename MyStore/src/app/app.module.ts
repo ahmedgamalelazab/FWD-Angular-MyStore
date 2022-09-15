98c7405d7ee7248ec2826454910a9f46446bee00
ahmedgamalelazab <agemy844@gmail.com>
2022-09-15 15:02:12 +0200

    fixed Base Module

--- MyStore/src/app/app.module.ts
+++ MyStore/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { ProductModule } from './product/product.module';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
